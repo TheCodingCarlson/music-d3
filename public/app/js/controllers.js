eac6dac220d720afe80add1cd70f5c9af29a6e21
Christian Carlson <chris.carlson.dev@gmail.com>
2016-03-07 20:16:32 -0800

    adding http request to last fm

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -1,6 +1,21 @@
 angular.module('MusicCtrls', [])
 .controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
 
+	$scope.searchMusic = function(term) {
+
+		$http({
+			url: 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + term + '&api_key=66d584518050d6a47dc9f9eedefd2a5c&format=json',
+			method: 'GET'
+		}).then(function(res) {
+			if(res.status === 200) {
+				$scope.albums = res.data;
+				console.log($scope.albums);
+			}
+		}, function(res) {
+			console.log(res);
+		});
+	}
+
 }]).controller('HistoryCtrl', ['$scope', function($scope) {
 
 }]);
